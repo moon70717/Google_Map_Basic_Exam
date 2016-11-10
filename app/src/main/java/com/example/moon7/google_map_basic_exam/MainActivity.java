6ebfc142dc776b67129931ea0c64ddcaa840906f
moon7 <moon70717@gmail.com>
2016-11-10 20:18:49 +0900

    구글맵 기본형식
    기본형식에 마커를 띄움

---
+++ app/src/main/java/com/example/moon7/google_map_basic_exam/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.moon7.google_map_basic_exam;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+    private GoogleMap mMap;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        SupportMapFragment mapFragment=(SupportMapFragment)getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+    @Override
+    public void onMapReady(GoogleMap googleMap){
+        mMap=googleMap;
+        LatLng sydney = new LatLng(37, 128);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("횡성입니다"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+}
